6f2e481234ab43435e47d30e2ef92529f24556e9
Ultron <duartemattias3@gmail.com>
2022-10-19 17:29:42 +0200

    pagina sigin

---
+++ pages/auth/signin.js
@@ -0,0 +1,78 @@
+import { signIn, getSession, getProviders } from "next-auth/react";
+import { FormLogin } from "../../components/login";
+import { Container, Text } from "@chakra-ui/react";
+
+import styled from "styled-components";
+import Head from "next/head";
+
+const SignIn = ({ providers }) => {
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Event Date - Iniciar sesion en la plataforma y comienza a disfrutar"
+        />
+        <title>Event Date | Login</title>
+      </Head>
+      <Section>
+        <Container maxW="container.lg">
+          <FormContainer>
+            <Text
+              bgGradient="linear(to-r, #fc382e, #FF0080)"
+              bgClip="text"
+              fontSize="6xl"
+              fontWeight="extrabold"
+              textAlign="center"
+            >
+              Event Date
+            </Text>
+            <FormLogin signIn={() => signIn(providers.google.id)} />
+          </FormContainer>
+        </Container>
+      </Section>
+    </>
+  );
+};
+const Section = styled.section`
+  padding: 4rem 0 1rem;
+  background-image: url("https://images.pexels.com/photos/7130475/pexels-photo-7130475.jpeg?auto=compress&cs=tinysrgb&w=1600");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  min-height: 100vh;
+`;
+
+const FormContainer = styled.div`
+  justify-content: center;
+  display: grid;
+  margin: 0 auto;
+  padding: 2rem;
+  background-color: rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(20px);
+  border-radius: 8px;
+  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.2);
+`;
+
+export async function getServerSideProps(ctx) {
+  const session = await getSession(ctx);
+  const providers = await getProviders();
+
+  if (session.user) {
+    return {
+      redirect: {
+        destination: "/profile/",
+      },
+    };
+  }
+
+  console.log(session)
+  return {
+    props: { 
+        providers,
+        session
+    },
+  };
+}
+
+export default SignIn;
