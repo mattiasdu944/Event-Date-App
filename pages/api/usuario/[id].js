d79aa5777082c44c173519163aec6fbd14d70595
Mattias <duartemattias3@gmail.com>
2022-10-31 20:38:41 -0400

    se agregaron las views

---
+++ pages/api/usuario/[id].js
@@ -0,0 +1,23 @@
+import { db } from "../../../database/db";
+
+export default async function handler(req, res){
+   
+    switch( req.method ){
+        case 'GET':
+            return getUserById(req, res) 
+        default:
+            return res.status(400).json({message:'Bad request'})
+    }
+}
+
+
+const  getUserById = async (req,res) => {
+    const {id} = req.query
+    
+    const [[user]] = await db.query("SELECT * FROM v_usuario_perfil WHERE id_usuario = ?", id); 
+    if( !user ){
+        return res.status(400).json({message:'Bad request'})
+    }
+
+    res.status(200).json(user);
+}
--- pages/api/usuario/index.js
+++ pages/api/usuario/index.js
@@ -1,26 +1,17 @@
 import { db } from "../../../database/db";
 
 export default async function handler(req, res){
-
-    console.log(req.body);
     switch( req.method ){
         case 'GET':
             return getDataUser(req, res);
+        default:
+            return res.status(400).json({message:'Bad request'})
     }
-
-    // const [[user]] = await db.query("SELECT * FROM usuarios WHERE usuarios.email = ?", 'duartemattias3@gmail.com');
-    // if(!user){
-    //     console.log('no hay a encontrado usuario');
-    // }
-    
-
-    res.status(200).json(user);
 }
 
 const getDataUser = async ( req, res ) => {
-    console.log(req);
+    // console.log(req);
     const [[user]] = await db.query("SELECT * FROM v_usuario_perfil WHERE id_usuario = ?",6); 
     res.status(200).json(user);
-    // return res.status(200).json( user )
 
 }
