34fb2a31c8a3d8b6b8c9012fec703d5b821a7957
Mattias <duartemattias3@gmail.com>
2022-11-02 20:16:12 -0400

    correccion

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -5,38 +5,38 @@ import { Layout } from "../../components/ui";
 import styled from "styled-components";
 import { Avatar, Box, Container, Flex, Text, WrapItem } from "@chakra-ui/react";
 
-const Profile = ({perfil}) => {
+const Profile = ({ perfil }) => {
   return (
     <Layout
       title='Perfil'
       description={`Perfil de ${perfil.name} en Event Date`}
     >
       <Section>
-          <Container maxW='container.lg'>
+        <Container maxW='container.lg'>
 
-            <Box display={{md:'flex'}} alignItems='center' justifyContent='center' gap={5} mb='3rem'>
-                <Box display='flex' alignItems='center' justifyContent='center'>
-                  <WrapItem  border='4px' borderColor='orange.500' rounded='full'>
-                    <Avatar size={{base:'xl', md:'2xl'}} name={perfil.name} src={perfil.image} />
-                  </WrapItem>
-                </Box>
-                <Box textAlign={{base:'center',md:'start'}}>
-                  <Text textStyle='h2' fontWeight={700}>
-                    {perfil.name}
-                  </Text>
-                  <Text>
-                    {perfil.email}
-                  </Text>
-                </Box>
+          <Box display={{ md: 'flex' }} alignItems='center' justifyContent='center' gap={5} mb='3rem'>
+            <Box display='flex' alignItems='center' justifyContent='center'>
+              <WrapItem border='4px' borderColor='orange.500' rounded='full'>
+                <Avatar size={{ base: 'xl', md: '2xl' }} name={perfil.name} src={perfil.image} />
+              </WrapItem>
             </Box>
-            <Box>
-              <Text textStyle='h2'>
-                Descripcion
+            <Box textAlign={{ base: 'center', md: 'start' }}>
+              <Text textStyle='h2' fontWeight={700}>
+                {perfil.name}
               </Text>
               <Text>
-                {perfil.descripcion === '' ? 'No hay descripcion de este perfil' : perfil.descripcion}
+                {perfil.email}
               </Text>
             </Box>
+          </Box>
+          <Box>
+            <Text textStyle='h2'>
+              Descripcion
+            </Text>
+            <Text>
+              {perfil.descripcion === '' ? 'No hay descripcion de este perfil' : perfil.descripcion}
+            </Text>
+          </Box>
         </Container>
       </Section>
 
@@ -53,11 +53,17 @@ const Section = styled.section`
 `
 
 export async function getServerSideProps(ctx) {
-  const {user} = await getSession(ctx)
-  const {data} = await usuarioApi.get(`/usuario/${user.id}`)
-  return {
-    props: {
-      perfil:data
-    },
+  try {
+    const { user } = await getSession(ctx)
+    const { data } = await usuarioApi.get(`/usuario/${user.id}`)
+    return {
+      props: {
+        perfil: data
+      },
+    }
+
+  } catch (error) {
+    console.log(error);
+    return;
   }
 }
