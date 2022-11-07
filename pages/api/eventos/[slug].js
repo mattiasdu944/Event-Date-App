7854aaaa40fb2c6baee9646f8fcc981144086b0a
Mattias <duartemattias3@gmail.com>
2022-11-07 10:11:04 -0400

    completando el rest

--- pages/api/eventos/[slug].js
+++ pages/api/eventos/[slug].js
@@ -16,13 +16,15 @@ const getEventBySlug = async (req, res) => {
     const { slug } = req.query
 
     const [[data]] = await db.query('SELECT * from v_data_evento WHERE slug = ?', slug) 
+
     if(!data){
         return res.status(400).json({message:'Evento no encontrado'})
-
     }
     
-    const [ticket] = await db.query('SELECT t.descripcion, t.precio from ticket as t WHERE t.evento = ?', data.event_id)
-    const evento = {...data, ticket}
+    const [ticket] = await db.query('SELECT t.descripcion, t.precio from ticket as t WHERE t.evento = ?', data.id)
+    const [[tempUser]] = await db.query('SELECT * from usuarios WHERE id = ?', data.usuario)
+    const {id, name, email, image} = tempUser;
+    const evento = {...data, ticket, user:{id, name, email, image}}
 
     return res.status(200).json(evento)
 }
